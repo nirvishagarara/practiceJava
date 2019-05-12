36462168f3135b3a37ec9c3198a58750d76be27f
Parth Kalavadia <kparth@amazon.com>
2019-05-11 20:54:41 -0700

    sanity check

--- Interface.java
+++ Interface.java
@@ -3,25 +3,24 @@ interface Animal {
 //   public void animalSound(); // interface method (does not have a body)
 //   public void sleep(); // interface method (does not have a body)
 //   public void color();
-     int age;
-     String name;
-     abstract void printName();
+     abstract public void printName();
 }
 
 // Pig "implements" the Animal interface
-class Pig implements Animal {
 
-      void printName() {
+
+
+
+class Interface implements Animal {
+  int age;
+  String name;
+
+      public void printName() {
         System.out.println("name is " +name);
         System.out.println("age is " +age);
     }
-}
-
-
-
-class Interface extends Animal {
   public static void main(String[] args) {
-    Pig myPig = new Pig();  // Create a Pig object
+    Interface myPig = new Interface();  // Create a Pig object
     myPig.name = "parth";
     myPig.age = 6;
     myPig.printName();
